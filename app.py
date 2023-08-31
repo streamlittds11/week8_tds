0d911e89b0d373eda55b0a63137d6a1c6613d798
streamlittds11 <143704962+streamlittds11@users.noreply.github.com>
2023-08-31 17:17:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ This app finds the largest among the 3 given numbers(value greater than the othe
 st.subheader('User Input Parameters')
 
 def user_input_features():
-    Number_1 = st.number_input("Number_1",min_value=-1000000000,max_value=1000000000)
+    Number_1 = st.number_input("Number_1")
     Number_2 = st.number_input("Number_2",min_value=-1000000000,max_value=1000000000)
     Number_3 = st.number_input("Number_3",min_value=-1000000000,max_value=1000000000)
     
